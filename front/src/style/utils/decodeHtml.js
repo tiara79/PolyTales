b81b7a9edf88347fe9eea0f26745898b7b1073fc
tiara79 <loverjj2@naver.com>
2025-08-15 12:53:14 +0900

    front 파일명/카멜 사용 제거를 위한 삭제

--- front/src/style/utils/decodeHtml.js
+++
@@ -1,6 +0,0 @@
-// src/utils/decodeHtml.js
-export default function decodeHtml(html) {
-  const txt = document.createElement('textarea');
-  txt.innerHTML = html;
-  return txt.value;
-}
--- front/src/style/utils/stripHtml.js
+++
@@ -1,5 +0,0 @@
-// src/style/utils/stripHtml.js
-export default function stripHtml(str = '') {
-   if (typeof str !== 'string') return '';
-      return str.replace(/<[^>]*>?/gm, '');
-}
