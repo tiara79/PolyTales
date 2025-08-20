3b281d7715282961abfa10b3fe6e2d4cbf11fd33
tiara79 <loverjj2@naver.com>
2025-08-20 11:33:25 +0900

    index.js에 북마크 주석

--- front/src/index.js
+++ front/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import Bookmark from './page/Bookmark'; // 경로가 맞음 (파일명 Bookmark.jsx, 폴더명 page)
+// import Bookmark from './page/Bookmark'; 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
